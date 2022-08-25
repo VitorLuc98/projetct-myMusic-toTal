b32ebe0f3a55597b5c14beaedb632d1dbaba17b6
VitorLuc98 <vbotelho@ciandt.com>
2022-08-25 11:50:58 -0300

    Implementacao do controller

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/impl/MusicServiceImpl.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/impl/MusicServiceImpl.java
@@ -6,6 +6,7 @@ import com.ciandt.summit.bootcamp2022.service.MusicService;
 import com.ciandt.summit.bootcamp2022.service.exceptions.ListIsEmptyException;
 import com.ciandt.summit.bootcamp2022.service.exceptions.NameLenghtException;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +17,7 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MusicServiceImpl implements MusicService {
 
     private final MusicRepository repository;
@@ -25,12 +26,11 @@ public class MusicServiceImpl implements MusicService {
     @Override
     @Transactional(readOnly = true)
     public List<MusicDto> findByMusicOrArtist(String name) {
-
-        if(Objects.isNull(name) || name.isEmpty()){
+        if (Objects.isNull(name) || name.isEmpty()) {
             return findAll();
         }
 
-        if(name.length() < 2 ){
+        if (name.length() < 2) {
             throw new NameLenghtException("The name should have more than 2 characters");
         }
 
@@ -39,14 +39,14 @@ public class MusicServiceImpl implements MusicService {
         if(list.isEmpty()){
             throw new ListIsEmptyException("Couldn't find any artist or song with the given name");
         }
-
         return list.stream()
                 .map(music -> modelMapper.map(music,MusicDto.class))
                 .collect(Collectors.toList());
     }
-    private List<MusicDto> findAll(){
-         return repository.findAll().stream()
-                .map(music -> modelMapper.map(music,MusicDto.class))
+
+    private List<MusicDto> findAll() {
+        return repository.findAll().stream()
+                .map(music -> modelMapper.map(music, MusicDto.class))
                 .collect(Collectors.toList());
     }
 }
