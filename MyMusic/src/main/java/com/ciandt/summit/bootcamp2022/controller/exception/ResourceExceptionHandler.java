67d6214598c913a04221f25d9f40d36b30e3cb3a
Diego Lopes Mercado <diegolm@ciandt.com>
2022-08-25 15:18:52 -0300

    [BOOT-900] - Criado exceptions

---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/exception/ResourceExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.ciandt.summit.bootcamp2022.controller.exception;
+
+import com.ciandt.summit.bootcamp2022.service.exceptions.NameLenghtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(NameLenghtException.class)
+    public ResponseEntity<StandardError> exceptionInvalidName(NameLenghtException e, HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        StandardError standardError = new StandardError();
+        standardError.setTimestamp(LocalDateTime.now());
+        standardError.setStatus(status.value());
+        standardError.setError(e.getMessage());
+        standardError.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(standardError);
+    }
+}
---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/exception/StandardError.java
@@ -0,0 +1,18 @@
+package com.ciandt.summit.bootcamp2022.controller.exception;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class StandardError implements Serializable {
+
+   private LocalDateTime timestamp;
+   private Integer status;
+   private String error;
+   private String path;
+}
