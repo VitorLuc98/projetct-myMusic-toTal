7298714569d8bfc55891f43e4b9eed1485d4fdb3
VitorLuc98 <vbotelho@ciandt.com>
2022-08-28 21:25:42 -0300

    [BOOT-910 Testes unitarios no controller]

---
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/controllers/MusicControllerTest.java
@@ -0,0 +1,125 @@
+package com.ciandt.summit.bootcamp2022.controllers;
+
+import com.ciandt.summit.bootcamp2022.dto.ArtistDto;
+import com.ciandt.summit.bootcamp2022.dto.MusicDto;
+import com.ciandt.summit.bootcamp2022.services.MusicService;
+import com.ciandt.summit.bootcamp2022.services.exceptions.NameLenghtException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MusicControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private MusicService service;
+
+    @Autowired
+    private MusicController musicController;
+
+    private ArtistDto artist1;
+    private MusicDto music1;
+    private ArtistDto artist2;
+    private MusicDto music2;
+    private ArtistDto artist3;
+    private MusicDto music3;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        MockitoAnnotations.openMocks(this);
+
+        artist1 = new ArtistDto("1", "Post Malone");
+        music1 = new MusicDto("1", "Take What You Want", artist1);
+
+        artist2 = new ArtistDto("2", "The Weeknd");
+        music2 = new MusicDto("2", "Save Your Tears", artist2);
+
+        artist3 = new ArtistDto("3", "Katy Perry");
+        music3 = new MusicDto("3", "Thinking of You", artist3);
+    }
+
+    @Test
+    public void findByNameOrMusicShouldReturnListMusicsWhenExistFilter() throws Exception {
+        var musics = List.of(music1, music2, music3);
+        var filtro = "You";
+
+        when(service.findByMusicOrArtist(filtro)).thenReturn(musics);
+
+        ResultActions result =
+                mockMvc.perform(get("/api/musicas")
+                        .param("filter", filtro)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$").isNotEmpty());
+        result.andExpect(jsonPath("$", hasSize(3)));
+    }
+
+    @Test
+    public void findByNameOrMusicShouldReturnListMusicsWhenExistFilterIgnoreCase() throws Exception {
+        var musics = List.of(music3);
+        var filtro = "kAtY";
+
+        when(service.findByMusicOrArtist(filtro)).thenReturn(musics);
+
+        ResultActions result =
+                mockMvc.perform(get("/api/musicas")
+                        .param("filter", filtro)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$").isNotEmpty());
+        result.andExpect(jsonPath("$", hasSize(1)));
+    }
+
+    @Test
+    public void findByNameOrMusicShouldReturnErrorWhenFilterIsLessThanTwoCharacters() throws Exception {
+        var filtro = "x";
+
+        when(service.findByMusicOrArtist(filtro)).thenThrow(new NameLenghtException());
+
+        ResultActions result =
+                mockMvc.perform(get("/api/musicas")
+                        .param("filter", filtro)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isBadRequest());
+        result.andExpect(jsonPath("$").isNotEmpty());
+        result.andExpect(jsonPath("$.status", is(400)));
+        result.andExpect(jsonPath("$.error", hasItem("The name should have more than 2 characters")));
+    }
+
+    @Test
+    public void findByNameOrMusicShouldReturnNoContentWhenListIsEmpty() throws Exception {
+        var filtro = "3xw4d3";
+
+        when(service.findByMusicOrArtist(filtro)).thenThrow(new NameLenghtException());
+
+        ResultActions result =
+                mockMvc.perform(get("/api/musicas")
+                        .param("filter", filtro)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isNoContent());
+    }
+
+}
