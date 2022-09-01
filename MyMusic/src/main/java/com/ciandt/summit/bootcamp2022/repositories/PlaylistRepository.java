8b54672e60d6ad0d6bc754b572416c957b090e5b
VitorLuc98 <108955597+VitorLuc98@users.noreply.github.com>
2022-09-01 08:44:00 -0300

    Update PlaylistRepository.java
    Adicionando o stereotype Repository

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/repositories/PlaylistRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/repositories/PlaylistRepository.java
@@ -2,6 +2,8 @@ package com.ciandt.summit.bootcamp2022.repositories;
 
 import com.ciandt.summit.bootcamp2022.model.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlaylistRepository extends JpaRepository<Playlist, String> {
 }
