9836fa8af295e5c606341b0160b884bb5d1d853b
VitorLuc98 <vbotelho@ciandt.com>
2022-09-06 15:08:38 -0300

    [BOOT-1258 - Mudando o método para void]

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/services/PlayListService.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/services/PlayListService.java
@@ -8,6 +8,6 @@ public interface PlayListService {
 
     PlaylistDto getPlaylistById(String id);
      PlaylistDto addMusicToPlaylist(String playlistId, MusicDto musicDto);
-     PlaylistDto removeMusicFromPlaylist(String playlistId, String musicaId);
+     void removeMusicFromPlaylist(String playlistId, String musicaId);
 }
 
