ed795bdf4c79d13e821abc170ed8981a79b1880c
VitorLuc98 <vbotelho@ciandt.com>
2022-09-06 14:10:28 -0300

    [BOOT-1110 - Ajuste para funcionamento dos testes]

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/config/interceptor/WebConfiguration.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/config/interceptor/WebConfiguration.java
@@ -1,14 +1,18 @@
 package com.ciandt.summit.bootcamp2022.config.interceptor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
 public class WebConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private TokenInterceptor interceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new TokenInterceptor())
+        registry.addInterceptor(interceptor)
                 .excludePathPatterns(
                         "/swagger-ui.html",
                         "/swagger-ui/**",
