760130c5f301e4be113ded8fdaab9ebdac5c5fc4
VitorLuc98 <vbotelho@ciandt.com>
2022-09-06 14:04:34 -0300

    [BOOT-923 - ajuste no configuracao web]

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/config/interceptor/WebConfiguration.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/config/interceptor/WebConfiguration.java
@@ -1,16 +1,18 @@
 package com.ciandt.summit.bootcamp2022.config.interceptor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
 public class WebConfiguration implements WebMvcConfigurer {
+    @Autowired
+    private TokenInterceptor tokenInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new TokenInterceptor())
+        registry.addInterceptor(tokenInterceptor)
                 .excludePathPatterns(
-                        "/swagger-ui.html",
                         "/swagger-ui/**",
                         "/v2/api-docs/**",
                         "/swagger-resources/**",
