0175a3c5c99ac14348b0a1e6727e4b47d2154705
VitorLuc98 <vbotelho@ciandt.com>
2022-09-07 19:32:29 -0300

    [BOOT-1259 - Adicionando tratamento de musica inexistente na playlist]

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controllers/exception/ResourceExceptionHandler.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controllers/exception/ResourceExceptionHandler.java
@@ -2,6 +2,7 @@ package com.ciandt.summit.bootcamp2022.controllers.exception;
 
 import com.ciandt.summit.bootcamp2022.config.interceptor.exceptions.UnauthorizedException;
 import com.ciandt.summit.bootcamp2022.services.exceptions.MusicExistInPlaylistException;
+import com.ciandt.summit.bootcamp2022.services.exceptions.MusicNotExistInPlaylistException;
 import com.ciandt.summit.bootcamp2022.services.exceptions.NameLenghtException;
 import com.ciandt.summit.bootcamp2022.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -13,16 +14,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
     @ExceptionHandler(NameLenghtException.class)
     public ResponseEntity<StandardError> exceptionInvalidName(HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
 
         StandardError standardError = new StandardError();
-        standardError.setTimestamp(LocalDateTime.now().toString());
+        standardError.setTimestamp(LocalDateTime.now().format(formatter));
         standardError.setStatus(status.value());
         standardError.setError("The name should have more than 2 characters");
         standardError.setPath(request.getRequestURI());
@@ -35,7 +39,7 @@ public class ResourceExceptionHandler {
         HttpStatus status = HttpStatus.BAD_REQUEST;
 
         StandardError standardError = new StandardError();
-        standardError.setTimestamp(LocalDateTime.now().toString());
+        standardError.setTimestamp(LocalDateTime.now().format(formatter));
         standardError.setStatus(status.value());
         standardError.setError(e.getMessage());
         standardError.setPath(request.getRequestURI());
@@ -49,7 +53,7 @@ public class ResourceExceptionHandler {
         HttpStatus status = HttpStatus.UNAUTHORIZED;
 
         StandardError standardError = new StandardError();
-        standardError.setTimestamp(LocalDateTime.now().toString());
+        standardError.setTimestamp(LocalDateTime.now().format(formatter));
         standardError.setStatus(status.value());
         standardError.setError("Unauthorized! Invalid credentials!");
         standardError.setPath(request.getRequestURI());
@@ -62,7 +66,20 @@ public class ResourceExceptionHandler {
         HttpStatus status = HttpStatus.BAD_REQUEST;
 
         StandardError standardError = new StandardError();
-        standardError.setTimestamp(LocalDateTime.now().toString());
+        standardError.setTimestamp(LocalDateTime.now().format(formatter));
+        standardError.setStatus(status.value());
+        standardError.setError(e.getMessage());
+        standardError.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(standardError);
+    }
+
+    @ExceptionHandler(MusicNotExistInPlaylistException.class)
+    public ResponseEntity<StandardError> musicNotExistInPlaylistException(MusicNotExistInPlaylistException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        StandardError standardError = new StandardError();
+        standardError.setTimestamp(LocalDateTime.now().format(formatter));
         standardError.setStatus(status.value());
         standardError.setError(e.getMessage());
         standardError.setPath(request.getRequestURI());
@@ -76,7 +93,7 @@ public class ResourceExceptionHandler {
 
         ValidationError err = new ValidationError();
 
-        err.setTimestamp(LocalDateTime.now().toString());
+        err.setTimestamp(LocalDateTime.now().format(formatter));
         err.setStatus(status.value());
         err.setError("Validation Error");
         err.setPath(request.getRequestURI());
