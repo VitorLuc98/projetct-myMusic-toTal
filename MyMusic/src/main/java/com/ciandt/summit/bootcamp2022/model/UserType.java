32b88a8d965ac9e90bb35af3234be39ced843cc7
VitorLuc98 <vbotelho@ciandt.com>
2022-09-10 22:51:16 -0300

    [BOOT-1332 - Criacao da classe UserType]

---
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/model/UserType.java
@@ -0,0 +1,19 @@
+package com.ciandt.summit.bootcamp2022.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class UserType{
+
+    private String id;
+    private String description;
+
+}
